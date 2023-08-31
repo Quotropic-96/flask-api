01f9188d2bcd0c2dcead69bdecd02a55b47ffadb
Gerard Solanes <gerardsolher@gmail.com>
2023-08-31 11:17:31 +0200

    Sets up flask app

---
+++ transactions-app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+transactions = [
+  { "sku": "T2006", "amount": "10.00", "currency": "USD" },
+  { "sku": "M2007", "amount": "34.57", "currency": "CAD" },
+  { "sku": "R2008", "amount": "17.95", "currency": "USD" },
+  { "sku": "T2006", "amount": "7.63", "currency": "EUR" },
+  { "sku": "B2009", "amount": "21.23", "currency": "USD" }
+]
+
+currency_rates = [
+  { "from": "EUR", "to": "USD", "rate": "1.359" },
+  { "from": "CAD", "to": "EUR", "rate": "0.732" },
+  { "from": "USD", "to": "EUR", "rate": "0.736" },
+  { "from": "EUR", "to": "CAD", "rate": "1.366" }
+]
